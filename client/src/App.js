aab44eb322e889593970a253343883f57289e174
Miguel <contact@debloat.us>
2022-03-07 13:18:47 -0500

    main routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,26 @@
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+
+
+import Navbar from "./components/navbar";
+import Home from "./pages/home";
+import Footer from "./components/footer";
+import Day from "./pages/day";
+import Login from "./pages/login";
+
+
+
 function App() {
+
   return (
-    <div>
-      Journal Appaaaa
-    </div>
+    <Router>
+      <Navbar/>
+      <Routes>
+        <Route exact path="/" element={<Home/>}/>
+        <Route path="/day" element={<Day/>}/>
+        <Route path="/login" element={<Login/>}/>
+      </Routes>
+      <Footer/>
+    </Router>
   );
 }
 
