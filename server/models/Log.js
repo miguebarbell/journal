1fb7184b78149648a3852f3c8d6f8318fa161bec
Miguel <contact@debloat.us>
2022-03-07 11:54:32 -0500

    added movement (duh!)

--- server/models/Log.js
+++ server/models/Log.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose');
 
 const LogSchema = new mongoose.Schema({
-	name:  {type: String, required: true},
+	user:  {type: String, required: true},
 	weight: {type: Number},
 	repetitions: {type: Number},
 	distance: {type: Number},
 	duration: {type: Number},
-	date: {type: Date, required: true}
+	date: {type: Date},
+	movement: {type: String, required: true}
 })
 
 module.exports = mongoose.model("Log", LogSchema)
