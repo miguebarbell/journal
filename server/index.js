794738b121d647f5011cf4cd07397ddc799cb130
Miguel <contact@debloat.us>
2022-03-07 11:53:52 -0500

    added log route

--- server/index.js
+++ server/index.js
@@ -6,11 +6,13 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 
 const authRoute = require('./routes/auth')
+const logRoute = require('./routes/log')
 
 const app = express();
 app.use(cors());
 app.use(express.json())
 app.use("/api/auth", authRoute)
+app.use("/api/log", logRoute);
 
 mongoose.connect(process.env.MONGO_URL)
 	.then(() => console.log('DB connection successful'))
