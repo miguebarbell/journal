c1938e71fd64a365c6846963cdf078eb23b75484
Miguel <contact@debloat.us>
2022-03-08 09:17:02 -0500

    reducers for the user v1

---
+++ client/src/redux/userRedux.js
@@ -0,0 +1,33 @@
+import {createSlice} from "@reduxjs/toolkit";
+
+const userSlice = createSlice({
+	name: "user",
+	initialState: {
+		currentUser: false,
+		error: false,
+		isFetching: false
+	},
+	reducers: {
+		loginStart: (state) => {
+			state.isFetching = true;
+		},
+		loginSuccess: (state, action) => {
+			state.currentUser = action.payload;
+			state.isFetching = false;
+		},
+		loginFailure: (state) => {
+			state.error = true;
+			state.isFetching = false;
+		},
+		logOut: (state) => {
+			state.currentUser = false;
+			state.error = false;
+		},
+		resetLogin: (state) => {
+			state.error = false;
+		}
+	}
+});
+
+export const { loginSuccess, loginFailure, logOut, loginStart, resetLogin} = userSlice.actions;
+export default userSlice.reducer;
