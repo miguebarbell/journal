ac8e0d26d247e2e6724b2600e4f9229a16e35603
Miguel <miguel@debloat.us>
2022-03-14 10:26:42 -0400

    type timeFrame start fields added

--- server/models/Goal.js
+++ server/models/Goal.js
@@ -3,8 +3,11 @@ const mongoose = require('mongoose');
 const GoalSchema = new mongoose.Schema({
 	user: { type: String, required: true},
 	movement: {type: String, required: true},
-	quantity: {type: String, required: true},
+	quantity: {type: Number, required: true},
 	unit: {type: String, required: true},
+	type: {type: String},
+	timeFrame: {type: Number},
+	start: {type: Date},
 	done: { type: Boolean, default: false},
 	gaveUp: { type: Boolean, default: false},
 	notes: {type: String},
