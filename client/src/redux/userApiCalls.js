e84245269da293bd15101373269c57e862ad0cdf
Miguel <miguel@debloat.us>
2022-03-16 11:32:40 -0400

    feat: setting goals to the store and database

--- client/src/redux/userApiCalls.js
+++ client/src/redux/userApiCalls.js
@@ -1,12 +1,18 @@
-import {loginFailure, loginSuccess, logOut, loginStart} from "./userRedux";
+import {loginFailure, loginSuccess, loginStart} from "./userRedux";
 import {publicRequest} from "../requestMethods";
+import {setGoals} from "./goalRedux";
 
+
+// user calls
 export const login = async (dispatch, user) => {
 	dispatch(loginStart());
 	try {
+		const goals = await publicRequest.post("api/goal/", user);
 		const res = await publicRequest.post("api/auth/login", user);
 		dispatch(loginSuccess(res.data));
+		dispatch(setGoals(goals.data));
 	} catch (err) {
+		console.log(err);
 		dispatch(loginFailure());
 	}
 };
@@ -17,5 +23,14 @@ export const register = async (dispatch, user) => {
 		login(dispatch, user);
 	} catch (err) {
 		dispatch(loginFailure());
+	}
+};
+
+export const edit = async (dispatch, user) => {
+
+	try{
+		await publicRequest.put("api/auth/", {user});
+	} catch (err) {
+		console.log(err);
 	}
 };
