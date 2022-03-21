61b9cc5077ebc015c7461fc071faccca71953a74
Miguel <miguel@debloat.us>
2022-03-21 11:26:23 -0400

    added a random bot avatar and a random inpirational quote

---
+++ client/src/components/quotes.js
@@ -0,0 +1,13 @@
+export const profileBanner = [
+	{text:'“To keep the body in good health is a duty – otherwise, we shall not be able to keep our mind strong and clear.”', author: 'Buddha'},
+	{text:'“Some people want it to happen, some wish it would happen, others make it happen.”' , author: 'Michael Jordan'},
+	{text:'“There’s no secret formula. I lift heavy, work hard, and aim to be the best.”' , author: 'Ronnie Coleman'}
+
+];
+
+export const afterALog = [
+	"“Success trains. Failure complains.”",
+	"“Look in the mirror. That’s your competition.”",
+	"“The sweat. The time. The devotion. It pays off.”",
+	"“You are the only one who can limit your greatness.”",
+];
--- client/src/pages/profile.jsx
+++ client/src/pages/profile.jsx
@@ -1,10 +1,13 @@
+// external libs
 import styled from "styled-components";
 import {useDispatch, useSelector} from "react-redux";
-
-import AddGoal from "../components/addgoal";
-import {threeColour} from "../conf";
 import {useState} from "react";
+// internal
+import AddGoal from "../components/addgoal";
 import {logOut} from "../redux/userRedux";
+// conf and variables
+import {threeColour} from "../conf";
+import {profileBanner} from "../components/quotes";
 
 
 const Container = styled.div``;
@@ -31,9 +34,37 @@ const LogoutButton = styled.button`
   }
 `;
 
+const InfoWrapper = styled.div`
+  max-height: 30vh;
+  display: flex;
+  //justify-content: flex-start;
+  align-items: center;
+  border: 1px solid black;
+  * {
+    margin: 1rem;
+  }
+`;
+const ProfilePicture = styled.img`
+  //border: 1px solid liner-gradient(blue, white);
+  border-radius: 50%;
+  height: 15vh;
+  width: 15vh;
+  background-image: linear-gradient(27deg, red, yellow)
+`;
 
+const MotivationWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+const Quote = styled.span``;
+const Author = styled.span`
+  font-size: 0.75rem;
+  font-weight: bold;
+`;
 
 const Profile = () => {
+  const motivationquote = profileBanner[Math.floor(Math.random() * profileBanner.length)];
   const dispatch = useDispatch();
   const handleLogout = () => {
     dispatch(logOut());
@@ -45,6 +76,8 @@ const Profile = () => {
   };
   const user = useSelector((state) => state.user.currentUser);
   const goals = useSelector((state) => state.training.goals);
+
+
   const daysLeft = (startDay, daysToComplete) => {
     const stringDays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
     const stringMonths = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
@@ -53,12 +86,19 @@ const Profile = () => {
     finishDay.setDate(beginDay.getDate() + daysToComplete);
     const daysLeft = Math.round((finishDay - new Date()) / (1000 * 60 * 60 * 24));
     return `${stringDays[finishDay.getDay()]} ${finishDay.getDate()}  / ${stringMonths[finishDay.getMonth()]} / ${finishDay.getFullYear()} --> ${daysLeft} days left`;
-
   };
+
     return (
         <Container>
+          <InfoWrapper>
+            <ProfilePicture src={`https://avatars.dicebear.com/api/bottts/${user.email}.svg`}/>
+            <Title>Hi {user.name}</Title>
+            <MotivationWrapper>
 
-          <Title>Hi {user.name}</Title>
+              <Quote>{motivationquote.text}</Quote>
+              <Author>{motivationquote.author}</Author>
+            </MotivationWrapper>
+          </InfoWrapper>
           <span>Here you can set new fitness goals and review your journey.</span>
           <ul>Goals
             <EditButton onClick={() => {addGoal();}}>ADD A NEW GOAL</EditButton>
