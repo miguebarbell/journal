97f6281393281a2d9df47d56e78b17ab930eab0f
Miguel <miguel@debloat.us>
2022-03-24 18:33:28 -0400

    changed to plural

--- server/models/Log.js
+++ server/models/Log.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const LogSchema = new mongoose.Schema({
 	user:  {type: String, required: true},
 	movement: {type: String, required: true},
-	rep: {type: Number, default: 1},
+	reps: {type: Number, default: 1},
 	sets: {type: Number, default: 1},
 	strain: {type: Number, default: 1},
 	unit: {type: String, required: true},
