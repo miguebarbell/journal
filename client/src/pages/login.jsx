9818edcdc8ca94b7f338ba6445c93f45fc5e3b87
Miguel <miguel@debloat.us>
2022-03-31 12:16:22 -0400

    better layout

--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -1,21 +1,52 @@
 import styled from "styled-components";
-import {COLOR_FOUR, COLOR_ONE, COLOR_THREE, COLOR_TWO} from "../conf";
+import {COLOR_FOUR, COLOR_ONE, COLOR_THREE, COLOR_TWO, PRIMARY, SECONDARY} from "../conf";
 import {useState} from "react";
 import {useDispatch, useSelector} from "react-redux";
 import {login} from "../redux/userApiCalls";
+import img from '../bg/highkick.jpg';
+import {Link} from "react-router-dom";
 
 export const Container = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
   min-height: 100vh;
-  background-color: ${COLOR_THREE};
   flex-direction: column;
+  background-image: linear-gradient(to bottom, ${PRIMARY + "50"}, ${SECONDARY + "50"}) , url(${({bg}) => bg});
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
 `;
 
+// export const GrayLayer = styled.div`
+//   height: 100%;
+//   width: 100%;
+//   background-image:
+// `;
+
 export const FormContainer = styled.form`
+  background-color: ${COLOR_THREE};
   border: 1px solid;
   display: flex;
+  div {
+    a {
+      text-decoration: none;
+    }
+    span {
+      padding: 10px;
+    }
+    #title {
+      color: ${COLOR_TWO};
+      font-weight: bold;
+      text-decoration: underline;
+    }
+    #subtitle {
+      color: ${COLOR_TWO};
+      &:hover {
+        text-decoration: underline;
+      }
+    }
+  }
   flex-direction: column;
   * {
     margin: 0.5rem;
@@ -29,9 +60,10 @@ export const Button = styled.button`
   background-color: ${COLOR_TWO};
   color: ${COLOR_FOUR};
   border: 2px solid ${COLOR_TWO};
-  padding: 0.5rem;
+  padding: 0.35rem;
   cursor: pointer;
   text-transform: uppercase;
+  font-weight: bold;
   &:hover {
     color: ${COLOR_TWO};
     background-color: ${COLOR_FOUR};
@@ -62,9 +94,11 @@ const Login = () => {
   };
   // console.log(useSelector(state => state.user));
     return (
-        <Container>
+        <Container bg={img}>
           <FormContainer>
-            LOGIN
+            <div>
+              <span id="title">Login</span> <Link to="/register"><span id="subtitle">Register</span></Link>
+            </div>
             <Input required placeholder="Username" onChange={(e) => setEmail(e.target.value)}/>
             <Input required type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)}/>
             <Button onClick={handleClick}>Login</Button>
--- client/src/pages/register.jsx
+++ client/src/pages/register.jsx
@@ -1,13 +1,17 @@
 import {Button, Container, FormContainer, Input} from "./login";
+import img from "../bg/Muay_Thai_Fight_Us_Vs_Burma.jpeg";
+import {Link} from "react-router-dom";
 
 const Register = () => {
   const handleClick = (e) => {
     e.preventDefault();
   };
     return (
-        <Container>
+        <Container bg={img}>
           <FormContainer>
-            Register
+            <div>
+              <span id="title">Register</span> <Link to="/login"> <span id="subtitle">Login</span></Link>
+            </div>
             <Input type="text" placeholder="email"/>
             <Input type="password" placeholder="password"/>
             <Input type="password" placeholder="confirm password"/>
