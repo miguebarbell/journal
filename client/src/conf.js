b967fa8d81ed2e37bccead999ca549f932f19185
Miguel <miguel@debloat.us>
2022-04-03 19:59:18 -0400

    more vibrant

--- client/src/conf.js
+++ client/src/conf.js
@@ -1,4 +1,3 @@
-
 // Dimension
 export const FOOTER_HEIGHT = '1.5rem';
 export const NAVBAR_HEIGHT = '3rem';
@@ -9,6 +8,6 @@ export const COLOR_TWO = '#14213D';
 export const COLOR_THREE = '#FCA311';
 export const COLOR_FOUR = '#E5E5E5';
 export const COLOR_FIVE = '#FFFFFF';
-export const PRIMARY = '#9400d3';
-export const SECONDARY = '#00FFFF';
+export const PRIMARY = '#d212ac';
+export const SECONDARY = '#00ffff';
 export const PRIMARY_DISABLED = '#36454f';
