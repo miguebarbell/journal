7cdc24d13ff917279c2ba21bd74d1d8c051af266
Miguel <miguel@debloat.us>
2022-04-05 21:03:39 -0400

    maybe with typescript next time

--- client/src/components/helper.js
+++ client/src/components/helper.js
@@ -10,12 +10,27 @@ export const checkDays = (date1, date2) => {
 };
 
 
+export const goalExpiration = (date, timeFrame) => {
+	// date: Date, timeFrame: int
+	// return the days left from today
+
+	const today = new Date();
+	let lastDay = new Date(date);
+	lastDay.setDate(lastDay.getDate() + timeFrame);
+	return {daysLeft: Math.floor((lastDay.getTime() - today.getTime())/(1000 * 3600 * 24)), expiration: lastDay};
+};
+
+export const prettierDate = (date) => {
+	// date: string
+	const humanReadable = new Date(date);
+	return `${humanReadable.getFullYear()}/${humanReadable.getMonth() + 1}/${humanReadable.getDate()}`;
+};
 
 // formulas for calculate the 1RM or relative intensity for the set, it returns a rounded value
 // eplayFormula for < 5RM, else brzyckiFormula
 export const epleyFormula = (strain, reps) => {
 	if (reps <= 1) return strain;
-	return Math.round(strain * (1 + (reps/30)));
+	return Math.round(strain * (1 + (reps / 30)));
 };
 
 export const brzyckiFormula = (strain, reps) => {
