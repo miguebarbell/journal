6db4f09793d3f9802806a1773a38c14f6f0b090b
Miguel <miguel@debloat.us>
2022-04-28 17:01:19 -0400

    feat: display for testing and accumulation goals

---
+++ client/src/components/goalChart.jsx
@@ -0,0 +1,207 @@
+import styled from "styled-components"
+import {useSelector} from "react-redux";
+import {Line} from "react-chartjs-2";
+import {
+	Chart as ChartJS,
+	CategoryScale,
+	LinearScale,
+	PointElement,
+	LineElement,
+	Title,
+	Tooltip,
+	Legend,
+} from 'chart.js';
+import {epleyFormula} from "./helper";
+
+ChartJS.register(
+	CategoryScale,
+	LinearScale,
+	PointElement,
+	LineElement,
+	Title,
+	Tooltip,
+	Legend
+);
+
+const Container = styled.div``
+const GoalChart = ({goal}) => {
+	const logsData = useSelector(state => state.training.logs)
+		.filter(log => log.movement === goal.movement)
+		.sort((a, b) => {
+			if (new Date(a.date) >= new Date(b.date)) return 1;
+			else return -1;
+		})
+
+
+	const options = {
+		responsive: true,
+		plugins: {
+			legend: {
+				position: 'top',
+			},
+			title: {
+				display: true,
+				text: goal.plan,
+			},
+		},
+	};
+
+	// const stringDays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+	const stringMonths = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+	const allDays = () => {
+		const arrayOfDays = [];
+		let workingMonth = 0;
+		for (let i = 0; i < logsData.length; i++) {
+			const firstDay = new Date(goal.start);
+			// should check every month change
+			// first day must be string indicating the day and month iex: May 20
+			if (i === 0) {
+				// 0 must be the initial goal date in the timeframe
+				arrayOfDays.push(`${stringMonths[firstDay.getMonth()]} ${firstDay.getDate()}`)
+				workingMonth = firstDay.getMonth()
+			}
+			// check the month change
+			const workingDay = new Date(logsData[i].date);
+			if (workingDay.getMonth() !== workingMonth) {
+				arrayOfDays.push(`${stringMonths[workingDay.getMonth()]} ${workingDay.getDate()}`)
+				workingMonth = workingDay.getMonth()
+			} else {
+				// check if this is the first day
+				if (workingDay.toDateString() === firstDay.toDateString()) continue;
+				// check if the last have the same day
+				if (`${workingDay.getDate()}` === arrayOfDays[arrayOfDays.length - 1]) continue;
+				arrayOfDays.push(`${workingDay.getDate()}`)
+			}
+		}
+		return arrayOfDays;
+	}
+	// console.log(allDays());
+	const accumulationForDay = () => {
+		const arrayOfAcummulatedStrain = [];
+		const arrayOfAcummulatedVolume = [];
+		const arrayOfMaxRelInt = [];
+		const arrayOfMaxAbsInt = [];
+		const firstDay = new Date(goal.start);
+		let accumulationForDay = {
+			date: firstDay,
+			value: 0,
+			relInt: 0,
+			absInt: 0
+		};
+		for (let i = 0; i < logsData.length; i++) {
+			const workingDay = new Date(logsData[i].date);
+			// no training the first day => push 0
+			if (i === 0 && firstDay.toDateString() !== workingDay.toDateString()) {
+				arrayOfAcummulatedStrain.push(accumulationForDay.value);
+				arrayOfAcummulatedVolume.push(accumulationForDay.value);
+				arrayOfMaxRelInt.push(accumulationForDay.relInt);
+				arrayOfMaxAbsInt.push(accumulationForDay.absInt);
+			}
+			if (workingDay.toDateString() === accumulationForDay.date.toDateString()) {
+				accumulationForDay.value += logsData[i].strain * logsData[i].sets * logsData[i].reps
+				if (logsData.length === 1) arrayOfAcummulatedStrain.push(accumulationForDay.value)
+				if (accumulationForDay.relInt < epleyFormula(logsData[i].strain, logsData[i].reps)) accumulationForDay.relInt = epleyFormula(logsData[i].strain, logsData[i].reps)
+				if (accumulationForDay.absInt < logsData[i].strain) accumulationForDay.absInt = logsData[i].strain
+				continue
+			} else {
+				accumulationForDay.date = workingDay
+				accumulationForDay.value = logsData[i].strain * logsData[i].sets * logsData[i].reps
+				accumulationForDay.relInt = epleyFormula(logsData[i].strain, logsData[i].reps)
+				accumulationForDay.absInt = logsData[i].strain
+			}
+			arrayOfAcummulatedStrain.push(accumulationForDay.value)
+			arrayOfAcummulatedVolume.push(accumulationForDay.value + arrayOfAcummulatedVolume[arrayOfAcummulatedVolume.length - 1])
+			arrayOfMaxRelInt.push(accumulationForDay.relInt)
+			arrayOfMaxAbsInt.push(accumulationForDay.absInt)
+		}
+		return {
+			dailyVol: arrayOfAcummulatedStrain,
+			accVol: arrayOfAcummulatedVolume,
+			dailyRelInt: arrayOfMaxRelInt,
+			dailyAbsInt: arrayOfMaxAbsInt
+		};
+	}
+	// const maxRelIntForDay = () => {
+	// 	const arrayOfMaxRelIntPerDay = [];
+	//
+	// }
+	// const maxRelAbsForDay = () => {
+	//
+	// }
+	// const accumulatedStrainForDay = () => {
+	// 	const arrayWithValuesPerDay = accumulationForDay().dailyVol
+	// 	let arrayWithAccumulated = []
+	// 	for (let i = 0; i < arrayWithValuesPerDay.length; i++) {
+	// 		if (i === 0) {
+	// 			arrayWithAccumulated.push(arrayWithValuesPerDay[i])
+	// 			continue
+	// 		}
+	// 		arrayWithAccumulated.push(arrayWithAccumulated[i-1] + arrayWithValuesPerDay[i])
+	//
+	// 	}
+	//
+	// 	return arrayWithAccumulated
+	// }
+	let data = {}
+	if (goal.plan === "accu") {
+		data = {
+			labels: allDays(),
+			datasets: [
+				{
+					label: 'Volume per day',
+					data: accumulationForDay().dailyVol,
+					backgroundColor: 'rgba(53, 162, 235, 0.5)',
+					borderColor: 'rgb(53, 162, 235)',
+				},
+				{
+					label: 'Accumulated',
+					data: accumulationForDay().accVol,
+					backgroundColor: 'rgba(53, 62, 235, 0.5)',
+					borderColor: 'rgb(53, 62, 235)',
+				},
+				{
+					label: 'Goal',
+					data: accumulationForDay().dailyVol.map(el => goal.quantity),
+					backgroundColor: 'rgba(255, 99, 132, 0.5)',
+					borderColor: 'rgb(255, 99, 132)',
+				},
+			],
+		};
+	}
+	else if (goal.plan === "test") {
+		data = {
+			labels: allDays(),
+			datasets: [
+				{
+					label: 'Max Abs Int / day',
+					data: accumulationForDay().dailyAbsInt,
+					backgroundColor: 'rgba(53, 62, 235, 0.5)',
+					borderColor: 'rgb(53, 62, 235)',
+				},
+				{
+					label: 'Max Rel Int / day',
+					data: accumulationForDay().dailyRelInt,
+					backgroundColor: 'rgba(53, 162, 235, 0.5)',
+					borderColor: 'rgb(53, 162, 235)',
+				},
+				{
+					label: 'Goal',
+					data: accumulationForDay().dailyVol.map(el => goal.quantity),
+					backgroundColor: 'rgba(255, 99, 132, 0.5)',
+					borderColor: 'rgb(255, 99, 132)',
+				},
+			],
+		};
+	}
+
+	return (
+		<Container>
+			<Line
+				data={data}
+				options={options}
+				type='bar'/>
+		</Container>
+	)
+}
+
+export default GoalChart
