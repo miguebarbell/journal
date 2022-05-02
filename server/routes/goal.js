76a6c8ae7f22972b5399175e94c868944106455d
Miguel <miguel@debloat.us>
2022-05-02 14:49:16 -0400

    api is updating the goal

--- server/routes/goal.js
+++ server/routes/goal.js
@@ -24,12 +24,38 @@ router.post("/add",
 	})
 
 router.post("/", async (req, res) => {
+	// return all goals for user
 	// console.log(req.body)
 	try {
 		const goals = await Goal.find({
 			"user": req.body.email
 		})
 		return res.status(200).json(goals)
+	} catch (err) {
+		return res.status(500).json(err)
+	}
+})
+
+router.put("/", async (req, res) => {
+	try {
+		console.log(req.body)
+		const goal = await Goal.updateOne({
+			movement: req.body.movement,
+			user: req.body.user,
+		},
+			{
+				$set: {
+					quantity: req.body.quantity,
+					unit: req.body.unit,
+					timeFrame: req.body.timeFrame,
+					start: req.body.start,
+					done: req.body.done,
+					gaveUp: req.body.gaveUp,
+					plan: req.body.plan,
+					notes: req.body.notes,
+				}
+			})
+		return res.status(200).json(goal)
 	} catch (err) {
 		return res.status(500).json(err)
 	}
