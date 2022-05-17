8091c3d0703af95a25e6dce54da23fbffc081a5f
Miguel R <miguel@debloat.us>
2022-05-17 19:27:23 -0400

    relint & accu

--- client/src/components/definitions.js
+++ client/src/components/definitions.js
@@ -1,6 +1,6 @@
 // this support the formulas and the ground of the app
-export const RELATIVE_INTENSITY = '"This is the definition for relative intensity."';
-export const DISTANCE_ACCUMULATED = '"This is the definition for the distance accumulated."';
+export const RELATIVE_INTENSITY = '"Its the calculus between your strain and the reps you did."';
+export const DISTANCE_ACCUMULATED = '"How much you accumulated."';
 export const SETS = '"How many times you did it with rest in between."';
 export const STRAIN = '"This is the weight, distance or time you want to measure"';
 export const REPS = '"How many times you did it without a rest."';
