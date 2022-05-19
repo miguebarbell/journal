e303c8f2b3b2b0e7f5fa66c2def6bbdb073b1b1f
Miguel R <miguel@debloat.us>
2022-05-19 11:42:32 -0400

    loading interface when is fetching

--- client/src/App.js
+++ client/src/App.js
@@ -10,24 +10,26 @@ import About from "./pages/about";
 import Profile from "./pages/profile";
 import Register from "./pages/register";
 import {useSelector} from "react-redux";
+import Loading from "./components/loading";
 
 
 
 function App() {
 
-  const user = useSelector((state) => state.user.currentUser);
+  const user = useSelector((state) => state.user);
 
   return (
     <Router>
+      {user.isFetching && <Loading/>}
       <Navbar/>
       <Routes>
-        <Route exact path="/" element={user ? <Home/> : <Login/>}/>
-        <Route path="/demo" element={user.email === 'demo@debloat.us' ? <Profile/> : <Login user={'demo'}/>}/>
-        <Route path="/day" element={user ? <Day/> : <Login/>}/>
-        <Route path="/login" element={user ? <Profile/> : <Login/>}/>
+        <Route exact path="/" element={user.currentUser ? <Home/> : <Login/>}/>
+        <Route path="/demo" element={user.currentUser.email === 'demo@debloat.us' ? <Profile/> : <Login user={'demo'}/>}/>
+        <Route path="/day" element={user.currentUser ? <Day/> : <Login/>}/>
+        <Route path="/login" element={user.currentUser ? <Profile/> : <Login/>}/>
         <Route path="/register" element={<Register/>}/>
         <Route path="/about" element={<About/>}/>
-        <Route path="/profile" element={user ? <Profile/> : <Login/>}/>
+        <Route path="/profile" element={user.currentUser ? <Profile/> : <Login/>}/>
       </Routes>
     </Router>
   );
